c6d17c409eacf0edade7b40216275b29ce028971
Pranoy Patra <96573528+pranoy1171999study@users.noreply.github.com>
2022-08-08 15:39:51 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 var apiUrl="http://pranoyspotifyclone.epizy.com/backend/apis";
-apiUrl="http://localhost/phppranoy/projects/Spotify_Clone/backend/apis";
+//apiUrl="http://localhost/phppranoy/projects/Spotify_Clone/backend/apis";
